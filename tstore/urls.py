77bab49c278b0665440713e7c70e52552db6403b
Arise Damilare <arisedamilare5@gmail.com>
2026-06-11 13:53:35 +0100

    django app first push

---
+++ tstore/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for tstore project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from productApp.views import homePageView, allProductView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", homePageView, name="home"),
+    path("all-product/", allProductView, name="all-product")
+    
+]
+
+# def get5():
+#     return 5
+
+# def add(a, b):
+#     b()
+    
+# add(5, get5)
