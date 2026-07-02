7e4a9a30baeb689f83a69fadee7dc6e2451c74cf
Arise Damilare <arisedamilare5@gmail.com>
2026-07-02 14:26:14 +0100

    Add user role and profile management: create Role and UserProfile models; implement role assignment on user creation.

--- userApp/models.py
+++ userApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class RoleChoices(models.TextChoices):
+    CUSTOMER = 'customer', 'Customer'
+    VENDOR = 'vendor', 'Vendor'
+    ADMIN = 'admin', 'Admin'
+    
+class Role(models.Model):
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    address = models.TextField()
+    roles = models.ManyToManyField(Role, blank=True, related_name="profiles")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            profile = UserProfile.objects.create(user=instance)
+            profile.roles.add(RoleChoices.CUSTOMER)
+            
+
+
